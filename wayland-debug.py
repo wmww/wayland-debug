7e7e5dd6b14893819e021a80f9c42351a71f289a
William Wold <wm@wmww.sh>
2018-11-07 09:10:47 -0800

    Now parses every argument type

--- wayland-debug.py
+++ wayland-debug.py
@@ -40,13 +40,32 @@ class WaylandObject:
         self.id = obj_id
 
     def __str__(self):
-        return color('1;34', '(' + str(self.id) + ': ' + self.type + ')')
+        return color('1;37', '(' + str(self.id) + ': ' + self.type + ')')
+
+class WaylandFd:
+    def __init__(self, val):
+        self.value = val
+
+    def __str__(self):
+        return color('36', 'fd ' + str(self.value))
 
 class WaylandArgument:
     def __init__(self, value_str):
         int_matches = re.findall('^-?\d+$', value_str)
         if int_matches:
             self.value = int(value_str)
+            return
+        float_matches = re.findall('^-?\d+(\.\d+)?([eE][+-]?\d+)?$', value_str)
+        if float_matches:
+            self.value = float(value_str)
+            return
+        nil_matches = re.findall('^nil$', value_str)
+        if nil_matches:
+            self.value = None
+            return
+        fd_matches = re.findall('^fd (\d+)$', value_str)
+        if fd_matches:
+            self.value = WaylandFd(int(fd_matches[0]))
             return
         str_matches = re.findall('^"(.*)"$', value_str)
         if str_matches:
@@ -63,18 +82,28 @@ class WaylandArgument:
             self.is_new = False
             return
         else:
-            self.value = None
-            self.value_str = value_str
+            class Unknown:
+                def __init__(self, val):
+                    self.str = val
+                def __str__(self):
+                    return repr(self.str)
+            self.value = Unknown(value_str)
 
     def __str__(self):
         if isinstance(self.value, int):
+            return color('1;34', str(self.value))
+        elif isinstance(self.value, float):
             return color('1;35', str(self.value))
         elif isinstance(self.value, str):
             return color('1;33', repr(self.value))
+        elif isinstance(self.value, WaylandFd):
+            return str(self.value)
         elif isinstance(self.value, WaylandObject):
-            return (color('1;32', 'new ') if self.is_new else '') + color('1;33', str(self.value))
+            return (color('1;32', 'new ') if self.is_new else '') + str(self.value)
+        elif self.value == None:
+            return color('37', 'null')
         else:
-            return color('1;31', repr(self.value) if self.value else repr(self.value_str))
+            return color('33', type(self.value).__name__ + ': ' + str(self.value))
 
 def parse_message_args(args_str):
     args = []
@@ -123,11 +152,12 @@ class WaylandMessage:
         self.args = message_args
 
     def __str__(self):
+        s = None
         return (
-            color('1;37', '{:10.4f}'.format(self.timestamp)) + ' ' +
+            color('37', '{:10.4f}'.format(self.timestamp)) + ' ' +
             str(self.obj) + ' ' +
-            color('1;36', self.name + ' [') +
-            color('1;36', ', ').join([str(i) for i in self.args]) + color('1;36', ']'))
+            color(s, self.name + ' [') +
+            color(s, ', ').join([str(i) for i in self.args]) + color(s, ']'))
 
 def main(input_file):
     while True:
