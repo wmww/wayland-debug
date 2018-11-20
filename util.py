7c3d87d2a1958c778e475b475d6a0b14263b9d17
William Wold <wm@wmww.sh>
2018-11-20 09:13:24 -0800

    Add output class

--- util.py
+++ util.py
@@ -48,6 +48,28 @@ def str_matches(pattern, txt):
     pattern = '^' + pattern + '$'
     return len(re.findall(pattern, txt)) == 1
 
+class Output:
+    def __init__(self, verbose, show_unprocessed, show_file, err_file):
+        self.verbose = verbose
+        self.show_unprocessed = show_unprocessed
+        self.out = show_file
+        self.err = err_file
+
+    def show(self, msg):
+        print(msg, file=self.out)
+
+    # Used when parsing WAYLAND_DEBUG lines and we come across output we can't parse
+    def unprocessed(self, msg):
+        if self.show_unprocessed:
+            self.show(color('37', ' ' * 10 + ' |  ' + line))
+
+    def log(self, msg):
+        if self.verbose:
+            print(color('37', 'wl log: ') + msg, file=self.out)
+
+    def warn(self, msg):
+        print(color('1;33', 'Warning: ') + msg, file=self.err)
+
 if __name__ == '__main__':
     print('File meant to be imported, not run')
     exit(1)
