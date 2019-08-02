19167c8374acf0579066324331b720c0a953055a
William Wold <wm@wmww.sh>
2019-08-02 21:00:35 +0000

    Test connection name generator

--- wl/test/test_wayland_data.py
+++ wl/test/test_wayland_data.py
@@ -33,3 +33,29 @@ class TestConnection(unittest.TestCase):
         self.assertTrue(isinstance(obj, Object))
         self.assertEquals(obj.type, 'wl_display')
         self.assertTrue(obj.resolved())
+
+    def test_name_generator(self):
+        gen = Connection.NameGenerator()
+        self.assertEquals(gen.next(), 'A')
+        self.assertEquals(gen.next(), 'B')
+        self.assertEquals(gen.next(), 'C')
+
+    def test_name_generator_big(self):
+        gen = Connection.NameGenerator()
+        self.assertEquals(gen.next(), 'A')
+        for i in range(22):
+            gen.next()
+        self.assertEquals(gen.next(), 'X')
+        self.assertEquals(gen.next(), 'Y')
+        self.assertEquals(gen.next(), 'Z')
+        self.assertEquals(gen.next(), 'AA')
+        self.assertEquals(gen.next(), 'AB')
+        for i in range(23):
+            gen.next()
+        self.assertEquals(gen.next(), 'AZ')
+        self.assertEquals(gen.next(), 'BA')
+        for i in range(23 + 26 * 24):
+            gen.next()
+        self.assertEquals(gen.next(), 'ZY')
+        self.assertEquals(gen.next(), 'ZZ')
+        self.assertEquals(gen.next(), 'AAA')
