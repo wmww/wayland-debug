f1a1f5d14a4050f10bcf6132471ea51c6b94a5b3
William Wold <wm@wmww.sh>
2020-01-20 15:54:49 -0500

    Add GDB integration tests

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 import pytest
 
@@ -17,6 +18,26 @@ def arguments(*args):
     main_path = 'main.py'
     assert os.path.isfile(main_path)
     return [main_path, *args]
+
+def bin_exists(name):
+    '''Checks if a program exists on the system'''
+    args = ['which', name]
+    sp = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    _, _ = sp.communicate()
+    return sp.returncode == 0
+
+def wayland_socket_exists():
+    xdg_runtime_dir = os.getenv('XDG_RUNTIME_DIR')
+    if not xdg_runtime_dir:
+        xdg_runtime_dir = '/run/user/1000'
+    return os.path.exists(xdg_runtime_dir + '/wayland-0')
+
+def raising_input_func(msg):
+    assert False, 'Input should not be requested'
+
+def test_bin_exists_works():
+    assert bin_exists('ls') == True
+    assert bin_exists('doesnotexist') == False
 
 def test_load_from_file_doesnt_crash():
     out, err = streams()
@@ -48,3 +69,20 @@ def test_load_from_file_with_break():
     main.main(out, err, args, input_func)
     assert 'get_registry' in out.buffer, 'output: ' + out.buffer
     assert 'create_surface' not in out.buffer, 'output: ' + out.buffer
+
+@pytest.mark.skipif(not bin_exists('weston-info'), reason='weston-info not installed')
+def test_gdb_plugin_without_running():
+    out, err = streams()
+    args = arguments('-g', 'weston-info', '-ex', 'q')
+    main.main(out, err, args, raising_input_func)
+
+@pytest.mark.skipif(not bin_exists('weston-info'), reason='weston-info not installed')
+@pytest.mark.skipif(not wayland_socket_exists(), reason='no Wayland compositor running')
+def test_gdb_plugin_runs():
+    '''
+    These tests look nice, but they don't seem to detect any errors inside GDB
+    Luckily we also have test_runner.py which does
+    '''
+    out, err = streams()
+    args = arguments('-g', 'weston-info', '--ex', 'r')
+    main.main(out, err, args, raising_input_func)
