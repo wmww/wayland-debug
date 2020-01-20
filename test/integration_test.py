dd290661c23a80bc1ffbcd2d26bf08d654b1ef69
William Wold <wm@wmww.sh>
2020-01-20 14:53:13 -0500

    Add more file loading integration tests

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 import main
 from output import stream
 
@@ -22,4 +24,22 @@ def test_load_from_file_shows_messages():
     args = ['-l', short_log_file()]
     input_func = lambda msg: 'q'
     main.main(out, err, args, input_func)
-    assert 'get_registry' in out.buffer
+    assert 'get_registry' in out.buffer, 'output: ' + out.buffer
+    assert 'create_surface' in out.buffer, 'output: ' + out.buffer
+
+def test_load_from_file_with_filter():
+    out, err = streams()
+    args = ['-l', short_log_file(), '-f', 'wl_compositor']
+    input_func = lambda msg: 'q'
+    main.main(out, err, args, input_func)
+    assert 'get_registry' not in out.buffer, 'output: ' + out.buffer
+    assert 'create_surface' in out.buffer, 'output: ' + out.buffer
+
+@pytest.mark.xfail(reason="see https://github.com/wmww/wayland-debug/issues/17")
+def test_load_from_file_with_break():
+    out, err = streams()
+    args = ['-l', short_log_file(), '-b', '[global]']
+    input_func = lambda msg: 'q'
+    main.main(out, err, args, input_func)
+    assert 'get_registry' in out.buffer, 'output: ' + out.buffer
+    assert 'create_surface' not in out.buffer, 'output: ' + out.buffer
