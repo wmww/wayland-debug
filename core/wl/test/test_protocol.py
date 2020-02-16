173668104edb2fd5e8b96a895865cd4afdb1bbf1
William Wold <wm@wmww.sh>
2020-02-16 11:22:28 -0500

    Add protocol tests

--- core/wl/test/test_protocol.py
+++ core/wl/test/test_protocol.py
@@ -10,6 +10,9 @@ def readme_path():
     return path.join(protocols_path(), 'README')
 
 class TestProtocol(unittest.TestCase):
+    def tearDown(self):
+        dump_all()
+
     def test_protocol_dir_exists(self):
         self.assertTrue(path.isdir(protocols_path()))
 
@@ -32,3 +35,37 @@ class TestProtocol(unittest.TestCase):
     def test_load_all(self):
         out = output.Strict()
         load_all(out)
+
+    def test_get_arg_fails_with_no_protocols_loaded(self):
+        self.assertEqual(get_arg('wl_surface', 'attach', 0), None)
+
+class TestLoadedProtocols(unittest.TestCase):
+    def setUp(self):
+        out = output.Strict()
+        load_all(out)
+
+    def tearDown(self):
+        dump_all()
+
+    def test_get_arg_succeeds(self):
+        arg = get_arg('wl_surface', 'attach', 0)
+        self.assertIsInstance(arg, Arg)
+
+    def test_get_arg_returns_different_args_for_message(self):
+        first = get_arg('wl_surface', 'attach', 0)
+        second = get_arg('wl_surface', 'attach', 1)
+        self.assertIsInstance(first, Arg)
+        self.assertIsInstance(second, Arg)
+        self.assertNotEqual(first, second)
+
+    def test_get_arg_returns_non_on_unknown_protocol(self):
+        arg = get_arg('not_a_real_protocol', 'attach', 0)
+        self.assertIs(arg, None)
+
+    def test_get_arg_errors_on_known_protocol_unknwon_message(self):
+        with self.assertRaises(RuntimeError):
+            get_arg('wl_surface', 'bad_message', 0)
+
+    def test_get_arg_errors_on_bad_arg_index(self):
+        with self.assertRaises(RuntimeError):
+            get_arg('wl_surface', 'attach', 4)
