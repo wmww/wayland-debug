549a59984e2e23f5e7afb59fa7537775140a516f
William Wold <wm@wmww.sh>
2020-08-14 15:13:23 -0400

    Put integration tests into a class and use better assertions

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,39 +1,41 @@
 import pytest
+import unittest
 
 from integration_helpers import *
 
-def test_bin_exists_works():
-    assert bin_exists('ls') == True
-    assert bin_exists('doesnotexist') == False
+class IntegrationTests(unittest.TestCase):
+    def test_bin_exists_works(self):
+        self.assertTrue(bin_exists('ls'))
+        self.assertFalse(bin_exists('doesnotexist'))
 
-def test_load_from_file_doesnt_crash():
-    run(['-l', short_log_file()])
+    def test_load_from_file_doesnt_crash(self):
+        run(['-l', short_log_file()])
 
-def test_load_from_file_shows_messages():
-    result = run(['-l', short_log_file()])
-    assert 'get_registry' in result, 'output: ' + result
-    assert 'create_surface' in result, 'output: ' + result
+    def test_load_from_file_shows_messages(self):
+        result = run(['-l', short_log_file()])
+        self.assertIn('get_registry', result)
+        self.assertIn('create_surface', result)
 
-def test_load_from_file_with_filter():
-    result = run(['-l', short_log_file(), '-f', 'wl_compositor'])
-    assert 'get_registry' not in result, 'output: ' + result
-    assert 'create_surface' in result, 'output: ' + result
+    def test_load_from_file_with_filter(self):
+        result = run(['-l', short_log_file(), '-f', 'wl_compositor'])
+        self.assertNotIn('get_registry', result)
+        self.assertIn('create_surface', result)
 
-@pytest.mark.xfail(reason='see https://github.com/wmww/wayland-debug/issues/17')
-def test_load_from_file_with_break():
-    result = run(['-l', short_log_file(), '-b', '[global]'])
-    assert 'get_registry' in result, 'output: ' + result
-    assert 'create_surface' not in result, 'output: ' + result
+    @pytest.mark.xfail(reason='see https://github.com/wmww/wayland-debug/issues/17')
+    def test_load_from_file_with_break(self):
+        result = run(['-l', short_log_file(), '-b', '[global]'])
+        self.assertIn('get_registry', result)
+        self.assertNotIn('create_surface', result)
 
-@pytest.mark.skipif(not bin_exists('weston-info'), reason='weston-info not installed')
-def test_gdb_plugin_without_running():
-    run(['-g', 'weston-info', '-ex', 'q'], error_on_input=True)
+    @pytest.mark.skipif(not bin_exists('weston-info'), reason='weston-info not installed')
+    def test_gdb_plugin_without_running(self):
+        run(['-g', 'weston-info', '-ex', 'q'], error_on_input=True)
 
-@pytest.mark.skipif(not bin_exists('weston-info'), reason='weston-info not installed')
-@pytest.mark.skipif(not wayland_socket_exists(), reason='no Wayland compositor running')
-def test_gdb_plugin_runs():
-    '''
-    These tests look nice, but they don't seem to detect any errors inside GDB
-    Luckily we also have test_runner.py which does
-    '''
-    run(['-g', 'weston-info', '--ex', 'r'], error_on_input=True)
+    @pytest.mark.skipif(not bin_exists('weston-info'), reason='weston-info not installed')
+    @pytest.mark.skipif(not wayland_socket_exists(), reason='no Wayland compositor running')
+    def test_gdb_plugin_runs(self):
+        '''
+        These tests look nice, but they don't seem to detect any errors inside GDB
+        Luckily we also have test_runner.py which does
+        '''
+        run(['-g', 'weston-info', '--ex', 'r'], error_on_input=True)
