2107ebc7ca50cc85b09422e710a72333df391b9e
William Wold <wm@wmww.sh>
2021-02-19 16:33:51 -0800

    Fail tests when error is logged

--- conftest.py
+++ conftest.py
@@ -1,1 +1,15 @@
 # If this file doesn't exist imports fail when using pytest
+
+import logging
+
+class CustomHandler(logging.NullHandler):
+    def __init__(self):
+        logging.NullHandler.__init__(self)
+        self.formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s');
+
+    def handle(self, record):
+        # According to https://docs.python.org/3/library/logging.html#levels >20 is more warning, error or critical
+        if record.levelno > 20:
+            raise AssertionError(self.formatter.format(record))
+
+logging.getLogger().addHandler(CustomHandler())
