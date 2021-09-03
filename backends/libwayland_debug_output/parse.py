f344debadedfcc08439ec1cd6400df444922788f
William Wold <wm@wmww.sh>
2021-09-03 10:38:24 -0700

    Support numbers with commas

--- backends/libwayland_debug_output/parse.py
+++ backends/libwayland_debug_output/parse.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 import interfaces
 from core import wl
@@ -9,7 +10,7 @@ class WlPatterns:
 
     def __init__(self):
         int_re = r'(?P<int>-?\d+)'
-        float_re = r'(?P<float>-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)'
+        float_re = r'(?P<float>-?\d+(?:[\.,]\d+)?(?:[eE][+-]?\d+)?)'
         fd_re = r'(?:fd (?P<fd>\d+))'
         str_re = r'(?:"(?P<str>.*)")'
         new_id_re = r'(?:new id (?:(?P<new_type>\w+)|(?:\[unknown\]))@(?P<new_id>\d+))'
@@ -27,8 +28,7 @@ class WlPatterns:
             array_re + '|' +
             fd_re + r')$')
         self.arg_re = re.compile(all_args_re)
-        self.any_arg_re = re.compile(r'((?<=,)|^)\s*("[^"]*"|[^,]+)($|(?=,))')
-        timestamp_regex = r'\[(\d+\.\d+)\]'
+        timestamp_regex = r'\[(\d+[\.,]\d+)\]'
         message_regex = r'(\w+)@(\d+)\.(\w+)\((.*)\)$'
         self.out_msg_re = re.compile(timestamp_regex + '  -> ' + message_regex)
         self.in_msg_re = re.compile(timestamp_regex + ' ' + message_regex)
@@ -56,15 +56,39 @@ def argument(p, value_str):
         elif match.group('str'):
             return wl.Arg.String(match.group('str'))
         elif match.group('float'):
-            return wl.Arg.Float(float(value_str))
+            return wl.Arg.Float(float(value_str.replace(',', '.')))
         elif match.group('fd'):
             return wl.Arg.Fd(int(match.group('fd')))
         elif match.group('array'):
             return wl.Arg.Array()
     return wl.Arg.Unknown(value_str)
 
-def argument_list(p, args_str):
-    return [argument(p, match[1]) for match in p.any_arg_re.findall(args_str)]
+def end_of_str(args_str: str, i: int) -> int:
+    i += 1
+    while i < len(args_str) and args_str[i] != '"':
+        if args_str[i] == '\\':
+            i += 1
+        i += 1
+    return i
+
+def argument_list_strs(args_str: str) -> List[str]:
+    result = []
+    i = 0
+    start = 0
+    while i < len(args_str):
+        if args_str[i:].startswith(', '):
+            result.append(args_str[start:i])
+            start = i + 2
+        if args_str[i] == '"':
+            i = end_of_str(args_str, i)
+        i += 1
+    if i != start:
+        result.append(args_str[start:i])
+    return result
+
+def argument_list(p: WlPatterns, args_str: str) -> List[wl.Arg.Base]:
+    str_list = argument_list_strs(args_str)
+    return [argument(p, s) for s in str_list]
 
 def message(raw):
     p = WlPatterns.lazy_get_instance()
@@ -78,7 +102,7 @@ def message(raw):
         raise RuntimeError(raw)
     match = matches[0]
     assert isinstance(match, tuple), repr(match)
-    abs_timestamp = float(match[0]) / 1000.0
+    abs_timestamp = float(match[0].replace(',', '.')) / 1000.0
     type_name = match[1]
     obj_id = int(match[2])
     message_name = match[3]
