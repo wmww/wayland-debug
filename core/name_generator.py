d52103a2c3dd255fcf59b6467a0443edd451ca39
William Wold <wm@wmww.sh>
2021-11-15 10:21:45 -0800

    Add number_to_letter_id()

--- core/name_generator.py
+++ core/name_generator.py
@@ -1,15 +1,20 @@
 from typing import Optional, List
 
+def number_to_letter_id(value: int, caps: bool) -> str:
+    value += 1
+    result = ''
+    base = ord('A') if caps else ord('a')
+    while value > 0:
+        value -= 1
+        result = chr(value % 26 + base) + result
+        value //= 26
+    return result
+
 class NameGenerator:
     def __init__(self) -> None:
         self.index = 0
 
     def next(self) -> str:
-        self.index += 1
         value = self.index
-        result = ''
-        while value > 0:
-            value -= 1
-            result = chr(value % 26 + ord('A')) + result
-            value //= 26
-        return result
+        self.index += 1
+        return number_to_letter_id(value, True)
